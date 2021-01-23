226812ebfb8c75b4b1e3d816a426c5e2c23db0ad
Thierry WADJI <wadjith@yahoo.fr>
2021-01-23 14:54:29 +0100

    Configuration par une classe.
    La classe principale ou exécutable est utilisée pour la configuration. AppConfig est supprimé

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -3,23 +3,26 @@ package com.mycompany.dvdstore;
 import com.mycompany.dvdstore.controller.MovieController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@Configuration
+@ComponentScan(basePackages = {"com.mycompany.dvdstore.service","com.mycompany.dvdstore.repository.file"})
+@PropertySource("classpath:application.properties")
 public class App 
 {
     public static void main( String[] args )
     {
-        /* Lit le fichier xml et instancie toutes les classes qui y sont définies
-         et les mettres en mémoire au travers de la variable context.
-         Crétaion dynamique des objets par Spring ou IoC = Inversion of Control
-        */
-       // ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
         /* Charge le contexte à partir des configurations indiquées sur une classe */
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         System.out.println( " ************ ENREGISTRER LE FILM ***********" );
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
+    }
+
+    @Bean
+    public MovieController getMovieController() {
+        // Un autre moyen de créer dynamiquement les beans
+        return new MovieController();
     }
 }
