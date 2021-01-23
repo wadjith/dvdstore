21942abd5d1885da862ee14f6e84275eeb9d1dd2
Thierry WADJI <wadjith@yahoo.fr>
2021-01-23 15:37:53 +0100

    Passage à SpringBoot
    Suppression des annotations superflues pour ne conserver que  l'annotation @SpringBootApplication. Elle contient d'ailleurs les autres (@Configuration, @ComponentScan ...)

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -1,27 +1,19 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.*;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan(basePackages = {"com.mycompany.dvdstore.service","com.mycompany.dvdstore.repository.file"})
-@PropertySource("classpath:application.properties")
-public class App 
+@SpringBootApplication
+public class App
 {
     public static void main( String[] args )
     {
-        /* Charge le contexte à partir des configurations indiquées sur une classe */
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        /* Charge le contexte à partir de SpringBoot */
+        ApplicationContext context = SpringApplication.run(App.class);
         System.out.println( " ************ ENREGISTRER LE FILM ***********" );
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
-    }
-
-    @Bean
-    public MovieController getMovieController() {
-        return new MovieController();
     }
 }
